cafdd6344994971ea70ca0b53a59e4cf2739d75b
Kai Luo <gluokai@gmail.com>
2022-06-12 19:26:11 +0800

    Move common code to comm.go

---
+++ relayer/comm.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2022 Kai Luo <gluokai@gmail.com>. All rights reserved.
+
+package relayer
+
+import (
+	bytes "github.com/bzEq/bx/bytes"
+	core "github.com/bzEq/bx/core"
+)
+
+func createPackUnpackPassManagerBuilder() *core.PackUnpackPassManagerBuilder {
+	pmb := core.NewPackUnpackPassManagerBuilder()
+	pmb.AddPairedPasses(&bytes.Padding{}, &bytes.DePadding{})
+	pmb.AddPairedPasses(&bytes.Compressor{}, &bytes.Decompressor{})
+	pmb.AddPairedPasses(&bytes.RotateLeft{}, &bytes.DeRotateLeft{})
+	return pmb
+}
+
+func createProtocol(name string) core.Protocol {
+	pmb := createPackUnpackPassManagerBuilder()
+	switch name {
+	case "raw":
+		return nil
+	case "variant":
+		vp := core.NewVariantProtocol()
+		return vp.Add(&core.ProtocolWithPass{
+			P:  &core.LVProtocol{},
+			UP: pmb.BuildUnpackPassManager(),
+			PP: pmb.BuildPackPassManager(),
+		}).Add(&core.ProtocolWithPass{
+			P:  &core.HTTPProtocol{},
+			UP: pmb.BuildUnpackPassManager(),
+			PP: pmb.BuildPackPassManager(),
+		})
+	default:
+		return &core.ProtocolWithPass{
+			P:  &core.HTTPProtocol{},
+			UP: pmb.BuildUnpackPassManager(),
+			PP: pmb.BuildPackPassManager(),
+		}
+	}
+}
+
+func CreateProtocol(name string) core.Protocol { return createProtocol(name) }
--- relayer/socks5.go
+++ relayer/socks5.go
@@ -7,45 +7,9 @@ import (
 	"math/rand"
 	"net"
 
-	bytes "github.com/bzEq/bx/bytes"
 	core "github.com/bzEq/bx/core"
 	socks5 "github.com/bzEq/bx/socks5"
 )
-
-func createPackUnpackPassManagerBuilder() *core.PackUnpackPassManagerBuilder {
-	pmb := core.NewPackUnpackPassManagerBuilder()
-	pmb.AddPairedPasses(&bytes.Padding{}, &bytes.DePadding{})
-	pmb.AddPairedPasses(&bytes.Compressor{}, &bytes.Decompressor{})
-	pmb.AddPairedPasses(&bytes.RotateLeft{}, &bytes.DeRotateLeft{})
-	return pmb
-}
-
-func createProtocol(name string) core.Protocol {
-	pmb := createPackUnpackPassManagerBuilder()
-	switch name {
-	case "raw":
-		return nil
-	case "variant":
-		vp := core.NewVariantProtocol()
-		return vp.Add(&core.ProtocolWithPass{
-			P:  &core.LVProtocol{},
-			UP: pmb.BuildUnpackPassManager(),
-			PP: pmb.BuildPackPassManager(),
-		}).Add(&core.ProtocolWithPass{
-			P:  &core.HTTPProtocol{},
-			UP: pmb.BuildUnpackPassManager(),
-			PP: pmb.BuildPackPassManager(),
-		})
-	default:
-		return &core.ProtocolWithPass{
-			P:  &core.HTTPProtocol{},
-			UP: pmb.BuildUnpackPassManager(),
-			PP: pmb.BuildPackPassManager(),
-		}
-	}
-}
-
-func CreateProtocol(name string) core.Protocol { return createProtocol(name) }
 
 type SocksRelayer struct {
 	Listen        func(string, string) (net.Listener, error)
