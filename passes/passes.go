7a4e4cf21e87edfc343a135d400dbe553bdea367
Kai Luo <gluokai@gmail.com>
2024-03-31 19:36:59 +0800

    Minor change

--- passes/passes.go
+++ passes/passes.go
@@ -189,7 +189,7 @@ func (self *OBFSEncoder) Run(b *iovec.IoVec) error {
 	if err := WrapLegacyPass(self, b); err != nil {
 		return err
 	}
-	l := (rand.Uint64()%64 + 7) & (uint64(127) << 3)
+	l := (rand.Uint64()%64 + 7) & (uint64(63) << 3)
 	var padding bytes.Buffer
 	for i := uint64(0); i < l/8; i++ {
 		binary.Write(&padding, binary.BigEndian, rand.Uint64())
