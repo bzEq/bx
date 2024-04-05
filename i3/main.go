a1264f123c678e3402f3541b15d35dd0197c3244
Kai Luo <gluokai@gmail.com>
2024-04-05 17:28:45 +0800

    Simplify i3 command line options

--- i3/main.go
+++ i3/main.go
@@ -6,7 +6,6 @@ package main
 
 import (
 	crand "crypto/rand"
-	"crypto/tls"
 	"encoding/binary"
 	"flag"
 	"io/ioutil"
@@ -15,7 +14,6 @@ import (
 	"net"
 	"strings"
 
-	"github.com/bzEq/bx/core"
 	"github.com/bzEq/bx/relayer"
 )
 
@@ -24,8 +22,6 @@ var options struct {
 	LocalUDP       string
 	LocalHTTPProxy string
 	Next           string
-	ProtocolName   string
-	UseTLS         bool
 }
 
 func startRelayer() {
@@ -34,33 +30,14 @@ func startRelayer() {
 	r.LocalUDP = options.LocalUDP
 	r.NumUDPMux = 4
 	r.LocalHTTPProxy = options.LocalHTTPProxy
-	r.RelayProtocol = options.ProtocolName
 	if options.Next != "" {
 		r.Next = strings.Split(options.Next, ",")
 	}
-	if options.UseTLS && !r.IsEndPoint() {
-		config := &tls.Config{InsecureSkipVerify: true, NextProtos: []string{options.ProtocolName}}
-		r.Dial = func(network, address string) (net.Conn, error) {
-			return tls.Dial(network, address, config)
-		}
-	} else {
-		r.Dial = func(network, address string) (net.Conn, error) {
-			return net.Dial(network, address)
-		}
+	r.Dial = func(network, address string) (net.Conn, error) {
+		return net.Dial(network, address)
 	}
-	if options.UseTLS && r.IsEndPoint() {
-		config, err := core.CreateBarebonesTLSConfig(options.ProtocolName)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		r.Listen = func(network, address string) (net.Listener, error) {
-			return tls.Listen(network, address, config)
-		}
-	} else {
-		r.Listen = func(network, address string) (net.Listener, error) {
-			return net.Listen(network, address)
-		}
+	r.Listen = func(network, address string) (net.Listener, error) {
+		return net.Listen(network, address)
 	}
 	r.Run()
 }
@@ -70,13 +47,11 @@ func main() {
 	binary.Read(crand.Reader, binary.BigEndian, &seed)
 	rand.Seed(seed)
 	var debug bool
+	flag.BoolVar(&debug, "debug", false, "Enable debug logging")
 	flag.StringVar(&options.Local, "l", "localhost:1080", "Listen address of this relayer")
 	flag.StringVar(&options.LocalUDP, "u", "", "UDP listen address of this relayer")
-	flag.StringVar(&options.Next, "n", "", "Address of next-hop relayer")
 	flag.StringVar(&options.LocalHTTPProxy, "http_proxy", "", "Enable this relayer serving as http proxy")
-	flag.StringVar(&options.ProtocolName, "proto", "", "Name of relay protocol")
-	flag.BoolVar(&options.UseTLS, "tls", false, "Wrap traffic in TLS")
-	flag.BoolVar(&debug, "debug", false, "Enable debug logging")
+	flag.StringVar(&options.Next, "n", "", "Address of next-hop relayer")
 	flag.Parse()
 	if !debug {
 		log.SetOutput(ioutil.Discard)
